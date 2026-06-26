f800ce2e5095973649c63f1b5fc9f169565e2d78
harperpaton-mmc <harperpaton@mmc.school.nz>
2026-06-26 14:43:39 +1200

    Simple number generator

---
+++ random_number_generator_v1.py
@@ -0,0 +1,12 @@
+# First interation of random number generator;
+# Generates a random number 1-50
+import random
+# Initialising variables
+question_type = ["kinetic energy", "gravitational energy"]
+
+# Main routine begins below
+random_1 = random.randint(1,50)
+random_2 = random.randint(1,50)
+random_3 = random.randint(1,50)
+
+print(random_1, random_2, random_3)
